658f1841aeb4b3998f0e3d1aa801872ca9623731
nizalovnd <ndn1@kent.ac.uk>
2026-02-09 11:45:29 +0000

    part 4

--- part_4.py
+++ part_4.py
@@ -1,1 +1,20 @@
+def rover_angle():
+    travel_log = []
+    while True:
+        s_angle = input("Sensor Reading (Slope Angle):")
+        try:
+            s_angle = int(s_angle)
+        except ValueError:
+            print("Sensor Glitch")
+            continue
+        if s_angle > 45:
+            print("CRITICAL TILT! HALTING.")
+            break
+        travel_log.append(s_angle)
+        print("Path Stable. Moving forward.")
+    print("Mission Terminated")
+    print(f"Total steps taken: {len(travel_log)}")
+    print(travel_log)
 
+if __name__ == "__main__":
+    rover_angle()
