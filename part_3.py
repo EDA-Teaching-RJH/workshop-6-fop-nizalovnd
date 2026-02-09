7a50aa3cb1074a23cd9e4db26e1e05f3d33ba7eb
nizalovnd <ndn1@kent.ac.uk>
2026-02-09 11:32:41 +0000

    part 3

--- part_3.py
+++ part_3.py
@@ -1,1 +1,24 @@
+def main():
+    try:
+        motor_speed = int(input("Enter Motor Speed:"))
+    except ValueError as e:
+        print("Error: Corrupted Signal. Maintaining current speed.")
 
+    x_coordinate = get_coordinate()
+
+
+def get_coordinate():
+    while True:
+        try:
+            x = int(input("Enter the X coordinate:"))
+        except ValueError:
+            print("Invalid Coordinate")
+            continue
+        if x > 100 or x < -100:
+            print("Coordinate out of range")
+            continue
+        break
+    return x
+
+if __name__ == "__main__":
+    main()
